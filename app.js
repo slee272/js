37dab63c518b9d4a1cb7e72fccfc8a457f6cce4b
slee272 <slee272@student.hywoman.ac.kr>
2021-12-27 16:16:06 +0900

    📝 update files / classList

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
 function handleTitleClick() {
-  if (h1.className === "active") {
-    h1.className = "";
-  } else {
-    h1.className = "active";
-  }
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
