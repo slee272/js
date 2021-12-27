16236b78490539cac2303d08f636b9e2822fd6f2
slee272 <slee272@student.hywoman.ac.kr>
2021-12-27 15:13:57 +0900

    📝 update js / if-else

--- app.js
+++ app.js
@@ -1,45 +1,14 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
-console.dir(h1);
-
 function handleTitleClick() {
-  h1.style.color = "blue";
+  const currentColor = h1.style.color;
+  let newColor;
+  if (currentColor === "blue") {
+    newColor = "tomato";
+  } else {
+    newColor = "blue";
+  }
+  h1.style.color = newColor;
 }
 
-const handle = () => {
-  h1.style.color = "blue";
-};
-
-// const sum = (a, b) => a + b;
-
-function handleMouseEnter() {
-  h1.innerText = "Mouse is here";
-}
-
-function handleMouseLeave() {
-  h1.innerText = "Mouse is gone";
-}
-
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-  alert("copy");
-}
-
-function handleWindowOffline() {
-  alert("offline");
-}
-function handleWindowOnline() {
-  alert("online");
-}
-
-h1.onclick = handle;
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
